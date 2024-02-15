7512568e2ec38a0fdb7df297920cbced3694b4e4
hanif.omar <hanif.omar@naazpremier.com.my>
2024-02-15 15:49:56 +0800

    chore: modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import SocialButton from "./components/SocialButton";
-import Navbar from "./components/navbar";
+import SocialButton from "../components/SocialButton";
+import Navbar from "../components/navbar";
 import Linkedin from "../../public/linkedin.svg";
 import Github from "../../public/github.svg";
 import Behance from "../../public/behance.svg";
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <section className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-4 mb-4">
+      <section className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-4 mb-4 h-[80vh] items-center justify-center">
         <div className="h-[30rem] rounded-3xl p-10 flex flex-col gap-16 bg-gradient-to-r from-rose-100 to-teal-100">
           <h1 className="text-4xl font-semibold">
             Hello, I'm a Frontend Developer with focus on react
@@ -22,16 +22,16 @@ export default function Home() {
           </p>
           <div className="flex flex-col lg:flex-row items-center gap-4 justify-self-end">
             <SocialButton bgColor="github">
-              <Github />
+              <Github className="h-5 w-5" />
             </SocialButton>
             <SocialButton bgColor="behance">
-              <Behance />
+              <Behance className="h-5 w-5" />
             </SocialButton>
             <SocialButton bgColor="linkedin">
-              <Linkedin />
+              <Linkedin className="h-5 w-5" />
             </SocialButton>
             <SocialButton bgColor="email">
-              <Email />
+              <Email className="h-5 w-5" />
             </SocialButton>
           </div>
         </div>
