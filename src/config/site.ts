f0dd65537ab228e426c3a5327975ffa5122cd0ac
Hanif Omar <hanif.omarr@gmail.com>
2024-08-07 13:00:17 +0800

    feat: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Hanif Omar",
+  url: "https://hanifomar.dev",
+  description: "",
+  author: "hanifomar",
+  links: {
+    github: "https://github.com/hanifomarr",
+    linkedIn: "https://www.linkedin.com/in/hanifomarr/",
+  },
+};
+
+export type siteConfig = typeof siteConfig;
