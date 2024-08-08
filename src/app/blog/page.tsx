f7e35f7a85f4614d6e0789955e2e080f3a4df357
Hanif Omar <hanif.omarr@gmail.com>
2024-08-08 17:17:00 +0800

    feat(ui): create blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,35 @@
+import { posts } from "#site/content";
+import { PostItem } from "@/components/post-item";
+
+const Blog = () => {
+  const displayposts = posts;
+
+  return (
+    <div className=" container max-w-4xl py-6 lg:py-10">
+      <div className=" flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className=" flex-1 space-y-4">
+          <h1 className=" inline-block font-black text-4xl lg:text-5xl">
+            Project
+          </h1>
+          <p className=" text-xl text-muted-foreground">description</p>
+        </div>
+      </div>
+      <hr className=" mt-8" />
+      {displayposts?.length > 0 ? (
+        <ul className=" flex flex-col">
+          {displayposts.map((post) => {
+            return (
+              <li key={post.slug}>
+                <PostItem {...post} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No Post</p>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/components/post-item.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Calendar } from "lucide-react";
+import { cn, formatDate } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+
+interface PostItemProps {
+  slug: string;
+  title: string;
+  description?: string;
+  date: string;
+}
+
+export function PostItem({ slug, title, description, date }: PostItemProps) {
+  return (
+    <article className=" flex flex-col gap-2 border-border border-b py-3">
+      <h2 className=" font-bold text-2xl">{title}</h2>
+      <p className=" max-w-none text-muted-foreground">{description}</p>
+      <div className=" flex items-center justify-between">
+        <dl>
+          <dt className=" sr-only">Published On</dt>
+          <dd className=" flex items-center gap-1 text-sm sm:text-base font-medium">
+            <Calendar className="h-4 w-4" />
+            <time dateTime={date}>{formatDate(date)}</time>
+          </dd>
+        </dl>
+        <Link
+          href={slug}
+          className={cn(buttonVariants({ variant: "link" }), "py-0")}
+        >
+          Read more →
+        </Link>
+      </div>
+    </article>
+  );
+}
